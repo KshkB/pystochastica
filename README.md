54ae9167957439abc1741b7b031f5fecbfd93626
Kowshik <99110419+KshkB@users.noreply.github.com>
2024-05-25 09:17:34 +1000

    updated README file'

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# pystochastica
+# PyStochastica
+
+Ever wanted to form arbitrary, linear and non-linear transformations on collections of random variables
+and study their properties? PyStochastica is an open source Python library tailored precisely for this! 
+It leverages SymPy to provide a framework in which to implement stochastic calculus of random variables.
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+if __name__ == '__main__':
+    setup(
+        name='pystochastica',
+        description='Computer alegbra systrem for stochastic (random) variables',
+        url='https://github.com/KshkB/pystochastica/',
+        author='Kowshik Bettadapura',
+        author_email='k.bettad@gmail.com',
+        py_modules=['discrete'],
+    )
